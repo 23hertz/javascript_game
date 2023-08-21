384d3980466470436dde74059c470f78b70e72f9
23hertz <57693962+23hertz@users.noreply.github.com>
2023-08-21 22:37:02 +0100

    adjustment

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ let randNum = Math.floor(Math.random() * 100)
 let dispRand = document.getElementById('displayRand');
 dispRand.innerHTML = `The rand Number is ${randNum}`
 let dispAttempts = document.getElementById('attempts')
-let totalAttempts = 5;
+let totalAttempts = 1;
+let answer = false
 
 btn.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -13,46 +14,24 @@ btn.addEventListener("submit", (e) => {
     let ans = document.getElementById('ans').value
     let dispAns = document.getElementById('displayAns');
     dispAns.innerHTML = `Your pick ${ans}`
+
     
     if(ans > randNum) 
     {
-      dispAttempts.innerHTML = `Total Attempt remaining ${totalAttempts --}`
+      dispAttempts.innerHTML = `Total Attempt remaining ${totalAttempts ++}`
+      answer = false
     }
     else if(ans < randNum)
     {
-      dispAttempts.innerHTML = `Total Attempt remaining ${totalAttempts --}`
+      dispAttempts.innerHTML = `Total Attempt remaining ${totalAttempts ++}`
+      answer = false
     }
     else
     {
-      console.log('you win')
+      answer = true
+      console.log(`you win and made ${totalAttempts}`)
     }
-
-
+    
 })
 
 
-// let noAttempts = 0;
-
-// let guessed = false;
-// 
-
-
-//  do {
-//   let question = Number(prompt("Enter a number"));
-//   console.log(`this is the raandom number: ${randNum}`);
-//   if (randNum < question) {
-//     console.log("Your value is greater");
-//     guessed = false;
-//   } else if (randNum > question) {
-//     console.log("Your value is lesser");
-//     guessed = false;
-//   } else {
-//     console.log("correct");
-//      guessed = true;
-
-//   }
-// } while (guessed === false);
-
-//  console.log(`The total no of attempt is: ${noAttempts}`);
-
-//   })
